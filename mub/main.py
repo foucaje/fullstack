a9f36d573cee9aa8d40da0a9c3254ad99d87b978
foucaje <jerome.foucauld@gmail.com>
2017-03-25 00:25:53 +0100

    finished implementing, ready for testing

--- mub/main.py
+++ mub/main.py
@@ -1,3 +1,9 @@
+""" A Multi-User-Blog Module for the Google App Engine
+
+This Module is developed along the Udacity FullStack NanoDegree
+by Jerome Foucauld in March 2017
+
+"""
 import os
 import webapp2
 import jinja2
@@ -6,18 +12,29 @@ import hmac
 
 from google.appengine.ext import db
 
+# Setting up the Jinja2 template modules
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
                                autoescape=True)
 
+# A Random string used as salt for hashing
 salt = 'Ad7Cd3smGf15p32z'
 
 
 def hash(value):
+    """Hashing function to create hash values
+    Argument:
+        value: a value to hash
+
+    Returns:
+        A hmac hashed string
+    """
     return hmac.new(salt, str(value)).hexdigest()
 
 
 class User(db.Model):
+    """ The User DB Model Class
+    """
     username = db.StringProperty(required=True)
     password = db.StringProperty(required=True)
     email = db.StringProperty()
@@ -25,6 +42,10 @@ class User(db.Model):
 
 
 class Post(db.Model):
+    """ The Post DB Model Class
+    References:
+        User Model 1:1
+    """
     subject = db.StringProperty(required=True)
     content = db.TextProperty(required=True)
     created = db.DateTimeProperty(auto_now_add=True)
@@ -33,19 +54,41 @@ class Post(db.Model):
 
 
 class Comment(db.Model):
+    """ The Comment DB Model Class
+    Comments to a Post
+
+    References:
+        User Model 1:1
+        Post Model 1:n
+    """
     created = db.DateTimeProperty(auto_now_add=True)
     content = db.TextProperty(required=True)
     user = db.ReferenceProperty(User)
-    post = db.ReferenceProperty(Post)
+    post = db.ReferenceProperty(Post, collection_name='comments')
+
+
+class Votes(db.Model):
+    """ The Votes DB Model Class
+    Votes (Likes) to a Post
+
+    References:
+        User Model 1:1
+        Post Model 1:n
+    """
+    post = db.ReferenceProperty(Post, collection_name='votes')
+    user = db.ReferenceProperty(User)
 
 
 class Handler(webapp2.RequestHandler):
+    """ The Parent webapp2 Request Handler
+    """
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
     def render_str(self, template, **params):
         t = jinja_env.get_template(template)
         params['user'] = self.user
+        params['recentposts'] = self.recentposts
         return t.render(params)
 
     def render(self, template, **kw):
@@ -56,9 +99,10 @@ class Handler(webapp2.RequestHandler):
         uid = self.request.cookies.get('uid')
         self.user = self.valid_cookie(str(uid))
 
-        p = Post.all().order('-created').fetch(limit=2)
-        self.recentposts = p
+        recentposts = Post.all().order('-created').fetch(limit=5)
+        self.recentposts = recentposts
 
+    # This functions validates the cookie data
     def valid_cookie(self, uid):
             if '|' in uid:
                 user_id = uid.split('|')[0]
@@ -67,18 +111,20 @@ class Handler(webapp2.RequestHandler):
                 if user_hash == hash(user_id):
                     u = User.get_by_id(int(user_id))
 
-                    if u:
-                        return u
+                    return u
 
+    # The login function to write a cookie
     def login(self, uid, hash_id):
         self.response.headers.add_header('Set-Cookie', 'uid=%s|%s; Path=/'
                                          % (str(uid), hash_id))
 
 
 class Blog(Handler):
+    """ The Blog Handler Class
+    Handles and renders the "Home" site showing all posts
+    """
     def get(self):
         posts = Post.all().order('-created')
-
         if posts:
             self.render('blog.html', posts=posts)
         else:
@@ -86,6 +132,9 @@ class Blog(Handler):
 
 
 class NewPost(Handler):
+    """ The new Post Handler Class
+    Handles and renders the new post page
+    """
     def get(self):
         if self.user:
             self.render('newpost.html')
@@ -96,10 +145,11 @@ class NewPost(Handler):
         subject = self.request.get('subject')
         content = self.request.get('content')
 
+        # Checking if content and subject (required) are available
         if subject and content:
             p = Post(content=content, subject=subject,
-                     user=self.user)
-            pkey = p.put().id()
+                     user=self.user).put()
+            pkey = p.id()
             self.redirect('/%d' % pkey)
 
         else:
@@ -111,37 +161,105 @@ class NewPost(Handler):
 
 
 class PermaLink(Handler):
+    """ The Permalink Handler Class
+    Handles and renders the a single post according the provided post_id
+    """
     def get(self, post_id):
         delete = self.request.get('delete')
         edit = self.request.get('edit')
+        vote = self.request.get('vote')
+        comment_id = self.request.get('comment_id')
 
-        if edit and self.user:
-            p = Post.get_by_id(int(post_id))
-            if p:
-                self.render('edit.html', subject=p.subject,
-                            content=p.content)
+        p = Post.get_by_id(int(post_id))
 
-        elif delete and self.user:
-            p = Post.get_by_id(int(post_id))
-            if p:
-                p.delete()
-                self.redirect('/')
+        if self.user:
+
+            # Checking if we are allowed to edit this post or comment
+            if edit:
+                if p and p.user.key() == self.user.key():
+
+                    if edit == 'post':
+                        self.render('edit.html', subject=p.subject,
+                                    content=p.content, post_id=post_id)
+
+                    elif edit == 'comment' and comment_id:
+                        upd_com = Comment.get_by_id(int(comment_id))
+                        self.render('post.html', post=p, comment_id=comment_id,
+                                    upd_com=upd_com.content)
+                else:
+                    self.render('post.html', post=p,
+                                error="You can only edit your own post and "
+                                      "comments!")
+
+            # Checking if we are allowed to delete this post
+            elif delete:
+                if p and p.user.key() == self.user.key():
+
+                    if delete == 'post':
+                        p.delete()
+                        self.redirect('/')
+
+                    elif delete == 'comment' and comment_id:
+                        Comment.get_by_id(int(comment_id)).delete()
+                        self.render('post.html', post=p,
+                                    success='Successfully deleted!')
+
+                else:
+                    self.render('post.html', post=p,
+                                error="You can only delete your own post!")
+
+            # Checking if we can vote for (like) this post
+            elif vote:
+                if p and p.user.key() != self.user.key():
+                    result = Votes.all().filter(
+                        'post =', p.key()).filter(
+                        'user =', self.user.key()
+                        ).get()
+
+                    if result:
+                        self.redirect('/')
+                        return
+                    else:
+                        Votes(post=p, user=self.user).put()
+                        self.redirect('/')
+                        return
+                else:
+                    self.redirect('/')
+            else:
+                self.render('post.html', post=p)
         else:
-            p = Post.get_by_id(int(post_id))
             self.render('post.html', post=p)
 
     def post(self, post_id):
         subject = self.request.get('subject')
         content = self.request.get('content')
+        comment = self.request.get('comment')
+        upd_com = self.request.get('upd_com')
+        upd_com_id = self.request.get('upd_com_id')
 
-        if subject and content and post_id:
-            p = Post.get_by_id(int(post_id))
-            if p and p.user.username == self.user.username:
+        p = Post.get_by_id(int(post_id))
+
+        # Updating the Post after edit, if the logged in user is also author
+        if subject and content and p:
+            if p.user.key() == self.user.key():
                 p.content = content
                 p.subject = subject
                 p.put()
 
-            self.redirect('/')
+            self.render('post.html', post=p, success='Successfully updated!')
+
+        # Adding a new comment
+        elif comment and self.user:
+            new = Comment(content=comment, user=self.user, post=p)
+            new.put()
+            self.render('post.html', post=p)
+
+        # Editing a comment
+        elif upd_com and upd_com_id and self.user:
+            update = Comment.get_by_id(int(upd_com_id))
+            update.content = upd_com
+            update.put()
+            self.render('post.html', post=p, success='Successfully updated!')
 
         else:
             error = 'You need to supply both!'
@@ -151,6 +269,9 @@ class PermaLink(Handler):
 
 
 class SignUp(Handler):
+    """ The SignUp Handler Class
+    Handles and renders the new user Signup page
+    """
     def get(self):
         self.render('signup.html')
 
@@ -165,15 +286,17 @@ class SignUp(Handler):
         MAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
         USER_ERR = PASS_ERR = VERIFY_ERR = EMAIL_ERR = ""
 
+        # Checking if the supplied values are valid
         if not username or not USER_RE.match(username):
-            USER_ERR = 'That is not a valid username!'
+            USER_ERR = 'The username you have entered is invalid!'
         if not password or not PASS_RE.match(password):
-            PASS_ERR = 'That is not a valid password!'
+            PASS_ERR = 'The password you have entered is invalid!'
         if not verify or (verify != password):
-            VERIFY_ERR = 'The Passwords do not match!'
+            VERIFY_ERR = 'The passwords do not match!'
         if email and not MAIL_RE.match(email):
-            EMAIL_ERR = 'The email is not valid!'
+            EMAIL_ERR = 'The email you have entered is not valid!'
 
+        # Checking for duplicate usernames
         if username:
             u = User.all().filter('username =', username).get()
             if u:
@@ -197,6 +320,9 @@ class SignUp(Handler):
 
 
 class Welcome(Handler):
+    """ The welcome Handler Class
+    Handles and renders the welcome page after login or signup
+    """
     def get(self):
         if self.user:
             self.render('welcome.html')
@@ -205,6 +331,9 @@ class Welcome(Handler):
 
 
 class Login(Handler):
+    """ The login Handler Class
+    Handles and renders the login page
+    """
     def get(self):
         self.render('login.html')
 
@@ -230,6 +359,9 @@ class Login(Handler):
 
 
 class Logout(Handler):
+    """ The logout Handler Class
+    Handles the user logout by clearing the cookie
+    """
     def get(self):
         self.response.headers.add_header('Set-Cookie', 'uid=; Path=/')
         self.redirect('/')
