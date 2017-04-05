ec2bae26e49fc68b94788322afcd1063d4d57003
foucaje <jerome.foucauld@gmail.com>
2017-04-05 21:12:20 +0200

    fixes to user checking

--- mub/main.py
+++ mub/main.py
@@ -32,19 +32,6 @@ def hash(value):
     return hmac.new(salt, str(value)).hexdigest()
 
 
-def login_required(func):
-    """
-    A decorator to confirm a user is logged in or redirect as needed.
-    """
-    def login(self, *args, **kwargs):
-        # Redirect to login if user not logged in, else execute func.
-        if not self.user:
-            self.redirect("/login")
-        else:
-            func(self, *args, **kwargs)
-    return login
-
-
 class User(db.Model):
     """ The User DB Model Class
     """
